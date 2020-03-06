651794737a379c1edfd6798b36811eeec3156810
vchirikov <vchirikov@servicetitan.com>
2020-03-07 02:08:46 +0300

    build: bump version

--- src/GoToDnSpy/source.extension.cs
+++ src/GoToDnSpy/source.extension.cs
@@ -9,9 +9,9 @@ namespace GoToDnSpy
     {
         public const string Id = "GoToDnSpy";
         public const string Name = "GoToDnSpy";
-        public const string Description = @"Open dnSpy and select method or type under caret.";
+        public const string Description = @"Open dnSpy or source.dot.net and select method or type under caret.";
         public const string Language = "en-US";
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
         public const string Author = "Vladimir Chirikov";
         public const string Tags = "GoToDnSpy, GoTo, Disasm, ILSpy, ILDisasm, Disassemble, CIL, IL, MSIL, dnSpy, de4dot, dnlib, decompile, Emit";
     }

b8566c687d130bdc8c50a2afccb5772829674295
Gamefreak130 <jirvin4523@gmail.com>
2020-11-08 15:20:32 -0600

    Make --dont-load-files optional & open in new tab

--- src/GoToDnSpy/SettingsDialog.cs
+++ src/GoToDnSpy/SettingsDialog.cs
@@ -18,6 +18,11 @@ namespace GoToDnSpy
         [Description("Path to dnSpy.exe. Example: C:\\dnSpy\\dnSpy.exe")]
         public string DnSpyPath { get; set;}
 
+        [Category("GoTo dnSpy")]
+        [DisplayName("Load previous list")]
+        [Description("Whether or not to load files other than those related to the selection, if a new process is started")]
+        public bool LoadPrevious { get; set; } = true;
+
         /// <summary>
         /// Mode was added recently in new version <see cref="DialogPage"/> ?
         /// So this is workaround for removing it from settings property grid
